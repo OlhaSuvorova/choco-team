0d2cd1f39d6b19f5c47d96621e1a4a3d9c3924e8
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-06-23 19:38:31 +0300

    new game 2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-import './js/modal';
-import './js/modal-bay-now';
-import './js/modal-about';
+import './js/all-modal';
+import './js/mobile-menu';
+import './js/swiper';
+
+// import './js/modal';
+// import './js/modal-bay-now';
+// import './js/modal-about';
+
